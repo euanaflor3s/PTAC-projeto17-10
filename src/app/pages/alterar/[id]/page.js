298a7d37e08f18e400dd885e08b67136968a8992
euanaflor3s <anacarlafloressilva@gmail.com>
2023-12-05 15:14:30 -0400

    const users

--- src/app/pages/alterar/[id]/page.js
+++ src/app/pages/alterar/[id]/page.js
@@ -2,45 +2,50 @@
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "../../global.css"
+import { useRouter } from "next/navigation";
+import { ToastContainer } from 'React-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Formulario = () => {
+export default alterar ({params}){
 
-  const handlerLogin = async (e) => {
-    e.preventDefault();  // cancelar um evento
-    toast.success('Alterado com sucesso')
-  }
+  const [user,setUser] = useState({
+    name:'',
+    email:'',
+    password:'',
+   });
 
-
-
-
+   const {push} =useRouter()
+ 
+   const handlerLogin = async (e) => {
+      e.preventDefault();  // cancelar um evento
+      toast.success('Alterado com sucesso')
+    }
+    
+    return (
+      <div className="body">
+        <h1>Página para alterar usuário</h1>
+        <form onSubmit={handlerLogin}>
   
-  return (
-    <div className="body">
-      <h1>Página para alterar usuário</h1>
-      <form onSubmit={handlerLogin}>
-
-  <div className= "sla">
-      <input  placeholder='nome' type="nome" className= "inputs" >
-        </input>
-        </div>
-
-        <div className= "sla">
-        <input  placeholder='E-mail' type="email" className= "inputs">
-        </input>
-  </div>
-
-  <div className= "sla">
-        <input placeholder='Senha'  type='password' className= "inputs" >
-        </input>
-        </div>
-
-        <button className='entrar'>Alterar</button> <button className='voltar'><a className='apvolt' href="/pages/dashboard">Voltar</a></button>
-        
-      </form>
-      <ToastContainer/>
+    <div className= "sla">
+        <input  placeholder='nome' type="nome" className= "inputs" >
+          </input>
+          </div>
+  
+          <div className= "sla">
+          <input  placeholder='E-mail' type="email" className= "inputs">
+          </input>
     </div>
-  )
-}
-
-export default Formulario;
+  
+    <div className= "sla">
+          <input placeholder='Senha'  type='password' className= "inputs" >
+          </input>
+          </div>
+  
+          <button className='entrar'>Alterar</button> <button className='voltar'><a className='apvolt' href="/pages/dashboard">Voltar</a></button>
+          
+        </form>
+        <ToastContainer/>
+      </div>
+    )
+  }
 
