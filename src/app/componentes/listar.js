a1182b6a20bd27b2a39da4752c71f9543bcb0190
euanaflor3s <anacarlafloressilva@gmail.com>
2023-12-05 15:56:27 -0400

    .

--- src/app/componentes/listar.js
+++ src/app/componentes/listar.js
@@ -1,10 +1,21 @@
+import link from "next/link"
 export default async function Listar({users}){
     await new Promise((resolve) => setTimeout(resolve,4000));
     return(
+
         <div>
             {users?.map((user, index)=>
-            <p key={index}>{user.nome}</p> 
+            <p key={index}>
+                <Link href={`/pages/alterar/${user.id}`}> {user.nome} 
+            
+            </Link></p> 
             )}
-            </div>
-    )
+        </div>
+  )
+
 }
+
+
+      
+        
+
